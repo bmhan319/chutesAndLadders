7657708a7059d258278aeb2a8b3c016a3ca37bde
Brian Han <bmhan319@hotmail.com>
2020-06-27 20:41:34 -0700

    finished spinner

--- js/spinner.js
+++ js/spinner.js
@@ -1,10 +1,25 @@
-function spin() {
-  let x = 1000
-  let y = 5000
+//Blue: 1 - 60
+//Yellow: 61 - 120
+//Purple: 121 - 180
+//Pink: 181 - 240
+//Orange: 241 - 300
+//Red: 301 - 360
 
-  let deg = Math.floor( Math.random() * (x - y) ) + y
+function spin() {
+  const spinButton = document.querySelector('.spinButton')
+  // min and max need a wide range in order for wheel to spin 
+  let min = 1024
+  let max = 9999
+  let deg = Math.floor( Math.random() * (max - min) + 1 ) + min
+  //let deg = Math.floor( Math.random() * (min - max) ) + max
 
   document.getElementById('box').style.transform = 'scale(1.3) rotate(' + deg + 'deg)'
+  spinButton.style.pointerEvents = 'none'
+  setTimeout( () => {spinButton.style.pointerEvents = 'auto'}, 3100)
 
-  console.log(Math.random() * -4000)
+
+  while (deg > 360) {
+    deg = deg - 360
+  }
+  return deg
 }
